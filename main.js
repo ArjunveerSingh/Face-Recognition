b485567b2e53615da6d4be56772adbd3decfb032
ArjunveerSingh <76784986+ArjunveerSingh@users.noreply.github.com>
2022-08-13 00:31:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera= document.getElementById("camera")
+Webcam.attach(camera);
+function ts(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="cid" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version:',ml5.version);
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/jV2S1TE-o/model.json',modelLoaded);
